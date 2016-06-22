13ff25871eeb7b78566afcc5c9fd5191c8d95abb
daiogo <dggf.93@gmail.com>
2016-06-21 22:35:04 -0300

    Submission version

--- proxy-server.rb
+++ proxy-server.rb
@@ -9,32 +9,32 @@ proxy_server_socket = TCPServer.open(PROXY_SERVER_PORT)
 
 # Makes proxy server run forever
 loop do
-
 	# Creates a new thread for each incoming request from clients
 	Thread.start(proxy_server_socket.accept) do |proxy_client_socket|
 
 		# Retrieves client's HTTP request
-		client_request = proxy_client_socket.gets
+		client_request = []
+		while line = proxy_client_socket.gets and line !~ /^\s*$/
+			# Transfers all request lines to client_request except for the
+			# Accept-Encoding line which prevents proxy from reading server's response
+			client_request << line.chomp unless line =~ /^Accept-Encoding:.*$/
+		end
 
-		# Uses regex to match URL
-		server_hostname = client_request.match(/([\da-z\.-]+)\.([a-z\.]{2,6})/).to_s
+		# Uses regex to match URL (client_request[1] is the line where the host is specified)
+		client_request_hostname = client_request[1].match(/([\da-z\.-]+)\.([a-z\.]{2,6})/).to_s
 
 		# Opens TCP connection with HTTP server on the matched URL
-		http_client_socket = TCPSocket.open(server_hostname, HTTP_SERVER_PORT)
+		http_client_socket = TCPSocket.open(client_request_hostname, HTTP_SERVER_PORT)
 
 		# Sends request to HTTP server
-		http_client_socket.puts "GET / HTTP/1.1"
-		http_client_socket.puts "Host: " + server_hostname
+		http_client_socket.puts client_request
 		http_client_socket.puts "\r\n\r\n"
 
 		# Prints HTTP response code and IP addresses
 		response_code = http_client_socket.gets
-		puts "------RESPONSE------"
-		puts response_code
-		puts "----PROXY SERVER ADDRESS----"
-		puts proxy_client_socket.peeraddr[3]
-		puts "----HTTP SERVER ADDRESS----"
-		puts http_client_socket.peeraddr[3]
+		puts "Response code: " + response_code
+		puts "Proxy server address: " + proxy_client_socket.peeraddr[3]
+		puts "HTTP server address: " + http_client_socket.peeraddr[3]
 		puts "\n"
 
 		# Gets HTTP server response
@@ -50,10 +50,11 @@ loop do
 			http_server_response = response_code + http_server_response
 		end
 
+		# Sends response to client
 		proxy_client_socket.puts http_server_response
 
+		# Close both sockets
 		http_client_socket.close
 		proxy_client_socket.close
-
 	end
 end
